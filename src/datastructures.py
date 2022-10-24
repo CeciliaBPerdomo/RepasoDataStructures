01f833f0476a81132ca38f50baf2b76a6afe70a2
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2022-10-24 17:25:45 +0000

    Final version

--- src/datastructures.py
+++ src/datastructures.py
@@ -11,8 +11,6 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
         self._members = [
         ]
 
@@ -26,20 +24,13 @@ class FamilyStructure:
         return self._members
 
     def delete_member(self, id):
-        #self._members = []
         for person in self._members:
-            print(person["id"])
             if id == person["id"]:
-                #self._members.append(member)
+                self._members.remove(person)
                 return self._members
-                #print(person["id"])
-                #personaBorrada = self._members.pop(id)
-                #return personaBorrada
-        # fill this method and update the return
     
 
     def get_member(self, id):
-        # fill this method and update the return
         for person in self._members:
             if id == person["id"]:
                 return person
